2ea458512937869c7797684a3c4d5dfbad1a998c
Dvis <gokhaledurgesh4@gmail.com>
2025-03-21 12:01:27 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -8,34 +8,47 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 async def progress_for_pyrogram(current, total, ud_type, message, start):
     now = time.time()
     diff = now - start
-    if round(diff % 5.00) == 0 or current == total:        
+    if round(diff % 5.00) == 0 or current == total:
         percentage = current * 100 / total
-        speed = current / diff
+        speed = current / diff if diff > 0 else 0
         elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
+        time_to_completion = round((total - current) / speed) * 1000 if speed else 0
         estimated_total_time = elapsed_time + time_to_completion
 
-        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+        elapsed_time_formatted = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time_formatted = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "{0}{1}".format(
-            ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
-            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))])
-        )            
-        tmp = progress + Txt.PROGRESS_BAR.format( 
-            round(percentage, 2),
-            humanbytes(current),
-            humanbytes(total),
-            humanbytes(speed),            
-            estimated_total_time if estimated_total_time != '' else "0 s"
-        )
+        # Fixed progress list jo user ne diya hai
+        progress_list = [
+            "[■□□□□□□□□□] 10%",
+            "[■■□□□□□□□□] 20%",
+            "[■■■□□□□□□□] 30%",
+            "[■■■■□□□□□□] 40%",
+            "[■■■■■□□□□□] 50%",
+            "[■■■■■■□□□□] 60%",
+            "[■■■■■■■□□□] 70%",
+            "[■■■■■■■■□□] 80%",
+            "[■■■■■■■■■□] 90%",
+            "[■■■■■■■■■■] 100%"
+        ]
+        
+        # Calculate index: percentage ko 10 se divide karke index nikalo, max index 9 hoga
+        index = min(int(percentage // 10), 9)
+        progress_str = progress_list[index]
+        
+        tmp = f"{progress_str}\n" \
+              f"{round(percentage, 2)}% complete\n" \
+              f"{humanbytes(current)} of {humanbytes(total)} at {humanbytes(speed)}/s\n" \
+              f"Time elapsed: {elapsed_time_formatted} | ETA: {estimated_total_time_formatted if estimated_total_time_formatted else '0 s'}"
+        
         try:
             await message.edit(
-                text=f"{ud_type}\n\n{tmp}",               
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✖️ Cancel ✖️", callback_data="close")]])                                               
+                text=f"{ud_type}\n\n{tmp}",
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✖️ Cancel ✖️", callback_data="close")]])
             )
-        except:
+        except Exception as e:
             pass
+
             
             
 
@@ -80,58 +93,3 @@ async def send_log(b, u):
             Config.LOG_CHANNEL,
             f"<b><u>New User Started The Bot</u></b> \n\n<b>User ID</b> : `{u.id}` \n<b>First Name</b> : {u.first_name} \n<b>Last Name</b> : {u.last_name} \n<b>User Name</b> : @{u.username} \n<b>User Mention</b> : {u.mention} \n<b>User Link</b> : <a href='tg://openmessage?user_id={u.id}'>Click Here</a>\n\nDate: {date}\nTime: {time}\n\nBy: {b.mention}"
         )
-
-
-# Token Verification
-
-TOKENS = {}
-VERIFIED = {}
-
-
-async def get_verify_shorted_link(link):
-    shortzy = Shortzy(api_key=API, base_site=URL)
-    link = await shortzy.convert(link)
-    return link
-
-async def check_token(bot, userid, token):
-    user = await bot.get_users(userid)
-    if user.id in TOKENS.keys():
-        TKN = TOKENS[user.id]
-        if token in TKN.keys():
-            is_used = TKN[token]
-            if is_used == True:
-                return False
-            else:
-                return True
-    else:
-        return False
-
-async def get_token(bot, userid, link):
-    user = await bot.get_users(userid)
-    token = ''.join(random.choices(string.ascii_letters + string.digits, k=7))
-    TOKENS[user.id] = {token: False}
-    link = f"{link}verify-{user.id}-{token}"
-    shortened_verify_url = await get_verify_shorted_link(link)
-    return str(shortened_verify_url)
-
-async def verify_user(bot, userid, token):
-    user = await bot.get_users(userid)
-    TOKENS[user.id] = {token: True}
-    tz = pytz.timezone('Asia/Kolkata')
-    today = date.today()
-    VERIFIED[user.id] = str(today)
-
-async def check_verification(bot, userid):
-    user = await bot.get_users(userid)
-    tz = pytz.timezone('Asia/Kolkata')
-    today = date.today()
-    if user.id in VERIFIED.keys():
-        EXP = VERIFIED[user.id]
-        years, month, day = EXP.split('-')
-        comp = date(int(years), int(month), int(day))
-        if comp<today:
-            return False
-        else:
-            return True
-    else:
-        return False
