79c9be21892e07d2ec57d528d04288e872844433
IamElite <elitemindbusiness@gmail.com>
2025-05-24 15:32:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,26 +1,26 @@
-import re, os, time
-id_pattern = re.compile(r'^.\d+$') 
+import os, time
 
 class Config(object):
-    # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
+    API_ID    = int(os.environ.get("API_ID", "0"))
     API_HASH  = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
-    # database config
     DB_NAME = os.environ.get("DB_NAME","DvisPappa")    
     DB_URL  = os.environ.get("DB_URL","")
  
-    # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://files.catbox.moe/4kwe69.jpg")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
     
-    # wes response configuration    
+    ADMIN = [
+        int(admin_id_str) 
+        for admin_id_str in os.environ.get('ADMIN', '').split() 
+        if admin_id_str.isdigit()
+    ] or [0]
+    
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
+    
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
-
 
 
 
